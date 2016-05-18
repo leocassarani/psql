5f7208fb68e24957e44448e1b328138695e5f654
Leo Cassarani <me@leocassarani.net>
2016-05-18 11:25:39 +0000

    Extract mathematical expressions and operations into a math.go file

--- select.go
+++ select.go
@@ -2,7 +2,6 @@ package psql
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
 )
 
@@ -36,66 +35,4 @@ func (s SelectQuery) ToSQL() string {
 // An Expression can be used in the SELECT list of an SQL query.
 type Expression interface {
 	ToSQLExpr() string
-}
-
-// IntLiteral wraps an integer n in an Expression that can be used to
-// build an SQL query.
-func IntLiteral(n int) intLiteral {
-	return intLiteral(n)
-}
-
-type intLiteral int
-
-func (i intLiteral) ToSQLExpr() string {
-	return strconv.Itoa(int(i))
-}
-
-// Plus returns an Expression representing the addition of Expressions a and b.
-func Plus(a, b Expression) binaryOp {
-	return binaryOp{a, b, plus}
-}
-
-// Minus returns an Expression representing the subtraction of Expression b from a.
-func Minus(a, b Expression) binaryOp {
-	return binaryOp{a, b, minus}
-}
-
-// Times returns an Expression representing the multiplication of Expression a and b.
-func Times(a, b Expression) binaryOp {
-	return binaryOp{a, b, times}
-}
-
-// Divide returns an Expression representing the division of Expression a and b.
-func Divide(a, b Expression) binaryOp {
-	return binaryOp{a, b, divide}
-}
-
-// Modulo returns an Expression representing the modulo of Expression a and b.
-func Modulo(a, b Expression) binaryOp {
-	return binaryOp{a, b, modulo}
-}
-
-// Modulo returns an Expression representing the exponentiation of Expression a and b.
-func Pow(a, b Expression) binaryOp {
-	return binaryOp{a, b, pow}
-}
-
-type binaryOpType string
-
-const (
-	plus   binaryOpType = "+"
-	minus               = "-"
-	times               = "*"
-	divide              = "/"
-	modulo              = "%"
-	pow                 = "^"
-)
-
-type binaryOp struct {
-	a, b   Expression
-	opType binaryOpType
-}
-
-func (o binaryOp) ToSQLExpr() string {
-	return fmt.Sprintf("(%s %s %s)", o.a.ToSQLExpr(), o.opType, o.b.ToSQLExpr())
 }
